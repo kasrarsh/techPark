2fd3c57be34756baeba8552f3370d49a3365239d
kasrarsh <kasramagers1377@gmail.com>
2020-02-02 15:07:46 +0330

    make hex responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
-// $(document).ready(function(){
-//     $(".rectangle").click(function(){
-//         $(this).addClass("active");
-//     });
-// });
+$(document).ready(function(){
+    $("#innovationCenter .rectangle").click(function(){
+        $(this).addClass("active");
+    });
+});
