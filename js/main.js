384f4eac1fb8f2156149272afb4cd4e8c478cfd8
kasrarsh <kasramagers1377@gmail.com>
2020-02-03 20:26:58 +0330

    swiper added+innovation part changed by clicking

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 
 $(document).ready(function(){
-    $("#innovationCenter .rectangle").click(function(){
+    $("#innovation-center .rectangle").click(function () {
+
+        var oldInnovationId =  $("#innovation-center .icons").find(".active").attr("innovationId");
+        $("#innovation-center .icons").find(".active").removeClass("active");
+        $("#innovation-center .text").find(".innovation-center-" + oldInnovationId).removeClass("d-block").addClass("d-none");
+        $("#innovation-center .image").find(".innovation-center-" + oldInnovationId).removeClass("d-block").addClass("d-none");
+
         $(this).addClass("active");
+        var newInnovationId = $(this).attr("innovationId");
+        $("#innovation-center .text").find(".innovation-center-" + newInnovationId).removeClass("d-none").addClass("d-block");
+        $("#innovation-center .image").find(".innovation-center-" + newInnovationId).removeClass("d-none").addClass("d-block");
     });
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
 });
